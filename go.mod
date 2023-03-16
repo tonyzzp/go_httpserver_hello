8c60b592933bdb8beacdd3736a0c0a842d3d5c9d
tonyzzp <tonyzzp@qq.com>
2023-03-16 15:36:24 +0800

    update

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module go_httpserver_hello
 
 go 1.20
+
+require golang.org/x/crypto v0.7.0
+
+require (
+	golang.org/x/net v0.8.0 // indirect
+	golang.org/x/text v0.8.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,10 +1,52 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	"golang.org/x/crypto/acme/autocert"
+)
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	var mux = &http.ServeMux{}
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("hello"))
 	})
-	http.ListenAndServe(":http", nil)
+	var ssl = os.Getenv("HTTP_SSL")
+	var host = os.Getenv("HTTP_HOST")
+	var email = os.Getenv("HTTP_EMAIL")
+	log.Println(os.Environ())
+	log.Println("ssl", ssl)
+	log.Println("host", host)
+	log.Println("email", email)
+	var hosts = strings.Split(host, ",")
+	log.Println("hosts", hosts)
+	if strings.ToLower(ssl) == "true" && len(host) > 0 {
+		var mgr = &autocert.Manager{
+			Prompt:     autocert.AcceptTOS,
+			Cache:      autocert.DirCache("/cert-cache"),
+			HostPolicy: autocert.HostWhitelist(hosts...),
+			Email:      email,
+		}
+		var server = &http.Server{
+			Addr:      ":https",
+			Handler:   mux,
+			TLSConfig: mgr.TLSConfig(),
+		}
+		var e = server.ListenAndServeTLS("", "")
+		if e != nil {
+			log.Fatalln(e)
+		}
+	} else {
+		var server = &http.Server{
+			Addr:    ":http",
+			Handler: mux,
+		}
+		var e = server.ListenAndServe()
+		if e != nil {
+			log.Fatalln(e)
+		}
+	}
 }
